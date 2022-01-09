d8076f9cf3dfce7de4687e5aa6189f2aedbb99d7
Yash Soni <yashsoni9977@gmail.com>
2022-01-09 19:21:23 +0530

    downloading all theme files

--- index.js
+++ index.js
@@ -1,13 +1,129 @@
 import {Shopify} from '@shopify/shopify-api';
 import dotenv from 'dotenv';
+import fs from 'fs';
 dotenv.config();
 
 const accesstoken = process.env.ACCESS_TOKEN;
 console.log(accesstoken);
 
-const client = new Shopify.Clients.Rest('aowlestore1.myshopify.com', accesstoken);
-const data = await client.get({
-  path: 'script_tags',
-});
 
-console.log(data);
+
+
+addCodeInThemeFile('aowlestore1.myshopify.com', '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');
+
+
+async function addCodeInThemeFile(shop, accessToken) {
+  try {
+    const client = new Shopify.Clients.Rest(shop, accessToken);
+
+    /**
+     * we are getting the published theme
+     */
+    const dataTheme = await client.get({  
+      path: "themes",
+      query: { role: "main" },
+    });
+
+    async function getAllData(dataTheme) {
+      var lol = await client.get({
+        path: `themes/${dataTheme.body.themes[0].id}/assets`
+      });
+      return [lol, dataTheme];
+    }
+
+    getAllData(dataTheme).then(
+     async (data) => {
+        console.log(data[0].body.assets);
+        var dataThemeID = data[1].body.themes[0].id;
+
+        let themePath = `./themes/${shop}_${dataThemeID}`;
+
+        fs.mkdir( themePath ,function(){
+          console.log("root folder has been created");
+        });
+
+
+        data[0].body.assets.forEach( async (element) => {
+
+          async function sleep(ms) {
+            setTimeout(()=>{}, ms);
+          }
+
+          await sleep(1500);
+            
+            let obj = {
+              path: `themes/${dataThemeID}/assets`,
+              query: { "asset[key]": element.key},
+            }
+            console.log(obj);
+
+            client.get(obj).then(
+              (data1) => {
+                let folderName = themePath + '/' + element.key.split("/")[0];
+                let fileName = folderName + '/' + element.key.split("/")[1];
+
+                console.log(folderName, fileName);
+
+                if (!fs.existsSync(folderName)) {
+                  fs.mkdir(folderName,function(){
+                    fs.writeFileSync(fileName,data1.body.asset.value);
+                  });
+                }
+                else{
+                  fs.writeFileSync(fileName,data1.body.asset.value);
+                }
+
+
+
+              },
+              (e) => console.log(e)
+            )
+        });
+      },
+      (e) => {
+        console.log(e);
+      }
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+    // need to work on response when something went wrong
+
+    // getData(dataTheme).then(
+    //   (data) => {
+    //     // const data1 = client.put({
+    //     //   path: `themes/${data[1].body.themes[0].id}/assets`,
+    //     //   data: {
+    //     //     asset: {
+    //     //       key: "layout/theme.liquid",
+    //     //       value: `${data[0].body.asset.value}`, //add your logic to edit code
+    //     //     },
+    //     //   },
+    //     //   type: DataType.JSON,
+    //     // });
+    //     // setTimeout(() => {
+    //     //   console.log("Theme Code has been updated");
+    //     // }, 5000);
+    //     console.log(data[0].body.asset.value);
+    //   },
+    //   (e) => {
+    //     throw e;
+    //   }
+    // );
+
+
+
+  } catch (err) {
+    return err;
+  }
+  return true;
+}
